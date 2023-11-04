f5afb71f3aee25e0a87f812f5ecc0a82bddb9b7b
Renz Rackhold <67743847+zernrack@users.noreply.github.com>
2023-11-04 18:00:04 +0800

    add todo controllers

---
+++ server/controllers/todoController.js
@@ -0,0 +1,37 @@
+const Todo = require("../models/todoModel");
+
+const getTodos = async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+};
+
+const createTodo = (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+  });
+
+  todo.save();
+
+  res.json(todo);
+};
+
+const deleteTodo = async (req, res) => {
+  const deletedTodo = await Todo.findByIdAndDelete(
+    req.params.id,
+    req.body.title
+  );
+  res.json(deletedTodo);
+};
+
+const toggleTodoStatus = async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.complete = !todo.complete;
+  todo.save();
+
+  res.json(todo);
+};
+
+exports.getTodos = getTodos;
+exports.createTodo = createTodo;
+exports.deleteTodo = deleteTodo;
+exports.toggleTodoStatus = toggleTodoStatus;
