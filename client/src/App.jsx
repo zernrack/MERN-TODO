a25f517c772449356fbd1fe8474608f1700b0697
Renz Rackhold <67743847+zernrack@users.noreply.github.com>
2023-11-11 00:29:52 +0800

    Apply tailwind classes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,9 @@ import axios from "axios";
 import { AiFillPlusSquare, AiFillDelete } from "react-icons/ai";
 import "./App.css";
 
-
 function App() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
-
-  // console.log("results:", import.meta.env.VITE_API_URL);
 
   useEffect(() => {
     getTodos();
@@ -51,36 +48,47 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col gap-10">
-        <div className="inline-flex items-center justify-center gap-3">
-          <div className="todo-input-wrapper">
-            <input
-              type="text"
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
-              placeholder="Add a new todo..."
-            />
-          </div>
-          <button type="button" onClick={handleAddTodo}>
-            <AiFillPlusSquare size={30} />
+      <div>
+        <p className="pt-10 text-5xl text-center">MERN TODO!</p>
+      </div>
+      <div className="p-8 bg-white rounded overflow-auto lg:w-[550px] md:w-[450px] w-[350px]  mx-auto m-10 border border-gray-700 min-h-[600px]">
+        <div className="flex mt-10">
+          <input
+            className="text-lg border border-gray-400 outline-none lg:w-[550px] md:w-[550px] w-96 rounded h-9 "
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            placeholder="Add a new todo..."
+          />
+          <button
+            className="cursor-pointer lg:w-9 h-9"
+            type="button"
+            onClick={handleAddTodo}
+          >
+            <AiFillPlusSquare size={38} />
           </button>
         </div>
 
-        <div className="flex flex-col items-center justify-center todo-list">
+        <div className="mt-8 text-xl">
           {!todos || !todos.length ? (
             <h3 className="text-center">No Todo Data!</h3>
           ) : (
             todos.map((todo) => (
-              <div className="inline-flex todo" key={todo._id}>
+              <div
+                className="relative lg:w-[448px] bg-white text-gray-500 p-3 mb-3 rounded border-gray-400 "
+                key={todo._id}
+              >
+                  <span
+                    onClick={() => handleTodoClick(todo._id)}
+                    className={
+                      "cursor-pointer " + (todo.complete ? "complete" : "")
+                    }
+                    id="todo-title"
+                  >
+                    {todo.title}
+                  </span>
                 <span
-                  onClick={() => handleTodoClick(todo._id)}
-                  className={todo.complete ? "complete" : ""}
-                  id="todo-title"
-                >
-                  {todo.title}
-                </span>
-                <span
-                  className="delete"
+                  className="absolute text-xl cursor-pointer text-custom-hsla right-3 top-3"
                   onClick={() => handleDeleteTodo(todo._id)}
                 >
                   <AiFillDelete size={25} />
