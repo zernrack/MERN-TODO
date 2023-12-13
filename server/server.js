b0bf39a010385b313209758caea34f528536e9a2
Renz Rackhold <67743847+zernrack@users.noreply.github.com>
2023-12-14 01:29:12 +0800

    Add comments

--- server/server.js
+++ server/server.js
@@ -7,15 +7,17 @@ const todoRoute = require("./routes/todoRoutes");
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); // Enable JSON parsing middleware
+app.use(cors()); // Enable CORS middleware
 
 async function startServer() {
   try {
     await connectDB();
 
+    // Use the todoRoute for requests starting with /api
     app.use("/api", todoRoute);
 
+    // Start the server
     app.listen(port, () => {
       console.log(`Server running on port ${port} 🔥`);
     });
